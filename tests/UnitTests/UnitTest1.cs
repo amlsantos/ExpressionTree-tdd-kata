06be4b843d47d7c6b6394b5322cf40ebb2290fd4
Andre Santos <6472330+santos-an@users.noreply.github.com>
2024-01-15 14:56:27 +0000

    green - empty expression

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,8 +6,34 @@ namespace UnitTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Model_EmptyExpression_Returns0()
     {
-        1.Should().Be(1);
+        // arrange
+        var emptyExpression = string.Empty;
+        var expression = new Expression();
+
+        // act
+        var root = expression.Model();
+        var result = root.Evaluate();
+
+        // assert
+        result.Should().Be(0);
+    }
+}
+
+public class Expression
+{
+    public Node Model()
+    {
+        var node = new Node();
+        return node;
+    }
+}
+
+public class Node
+{
+    public double Evaluate()
+    {
+        return 0;
     }
 }
