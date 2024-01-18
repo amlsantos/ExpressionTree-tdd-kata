6622ab51996b16cd983da3f45eeca7b8b6b94ded
Andre Santos <6472330+santos-an@users.noreply.github.com>
2024-01-18 15:13:17 +0000

    factory method

---
+++ src/Domain/Nodes/OperationFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Nodes;
+
+public static class OperationFactory
+{
+    public static OperationNode Create(string _operator, Node left, Node right)
+    {
+        return _operator switch
+        {
+            "+" => new AdditionNode(_operator, left, right),
+            "-" => new SubtractionNode(_operator, left, right),
+            "*" => new MultiplicationNode(_operator, left, right),
+            "/" => new DivisionNode(_operator, left, right),
+            _ => throw new InvalidOperationException()
+        };
+    }
+}
--- src/Domain/Nodes/OperationNode.cs
+++ src/Domain/Nodes/OperationNode.cs
@@ -14,19 +14,7 @@ public class OperationNode : Node
 
     public override double Evaluate()
     {
-        switch (_operator)
-        {
-            case "+":
-                return new AdditionNode(_operator, Left, Right).Evaluate();
-            case "-":
-                return new SubtractionNode(_operator, Left, Right).Evaluate();
-            case "*":
-                return new MultiplicationNode(_operator, Left, Right).Evaluate();
-            case "/":
-                return new DivisionNode(_operator, Left, Right).Evaluate();
-
-            default:
-                return 0;
-        }
+        var operation = OperationFactory.Create(_operator, Left, Right);
+        return operation.Evaluate();
     }
 }
